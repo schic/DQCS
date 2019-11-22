327fa924f81ee016b2d9a63be484c47ea99e07bd
yanqw <271853976@qq.com>
2019-11-22 16:52:41 +0800

    删除5.1.0

--- DataCleaner-DataCleaner-5.1.0/engine/xml-config/target/generated/configuration-src/org/datacleaner/configuration/jaxb/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.11 生成的
-// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2019.11.13 时间 02:22:46 PM CST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://eobjects.org/analyzerbeans/configuration/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.datacleaner.configuration.jaxb;
