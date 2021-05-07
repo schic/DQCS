3e3a2cb6cc50b08c087762849ada3777caf5ffa7
liudeyang <1064894600@qq.com>
2021-05-07 16:06:44 +0800

    更改目录结构

--- components/jleo/src/test/java/org/datacleaner/beans/JLeoTransformerTest.java
+++ components/jleo/src/test/java/org/datacleaner/beans/JLeoTransformerTest.java
@@ -1,12 +1,14 @@
 package org.datacleaner.beans;
-import org.apache.commons.lang.time.DateUtils;
+import jnr.ffi.Struct;
 import org.datacleaner.components.categories.LeoTransCategory;
 import org.datacleaner.data.MockInputColumn;
 import org.datacleaner.data.MockInputRow;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import static junit.framework.TestCase.assertEquals;
@@ -61,5 +63,62 @@ public class JLeoTransformerTest {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testTi(){
+        MyLinkedList obj = new MyLinkedList();
+        obj.addAtTail(1);
+        System.out.println(obj);
+        obj.get(0);
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testTi2(){
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node1 = new ListNode(1);
+        node4.next = node5;
+        node3.next = node4;
+        node2.next = node3;
+        node1.next = node2;
+        ListNode resNode = reverseList(node1);
+    }
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next==null){return head;}
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = head;
+        ListNode newDummy = getRes(dummy, pre);
+        newDummy = newDummy.next;
+        return newDummy;
+    }
+
+    private ListNode getRes(ListNode dummy, ListNode pre) {
+        //如果只有两个节点 互换即可
+        if (pre.next.next == null){
+            dummy.next = pre.next;
+            pre.next = null;
+            dummy.next.next = pre;
+            return  dummy;
+        }
+        //三个及三个以上的节点 首先置顶点
+        ListNode local = dummy.next;
+        ListNode topNode;
+        topNode = pre.next;
+        topNode.next = dummy.next;
+        dummy.next = topNode;
+        //裁剪pre
+        local = dummy.next;
+        while (local != pre){
+            local = local.next;
+        }
+        local.next = pre.next.next;
+        pre.next = pre.next.next;
+        getRes(dummy,pre);
+        return dummy;
     }
 }
